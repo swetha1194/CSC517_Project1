717ae4ad4276f4f63c08f3d590069a0d622a9550
Sindhu <sbalakr2@ncsu.edu>
2016-10-05 23:53:49 -0400

    Increased Session Timeout

--- OODD_1/config/initializers/session_store.rb
+++ OODD_1/config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_OODD_1_session', expire_after: 2.minutes
+Rails.application.config.session_store :cookie_store, key: '_OODD_1_session', expire_after: 10.minutes
 
