4b0e71ca2b7171e2a3e444b194e3190845c04da6
Sindhu <sbalakr2@ncsu.edu>
2016-10-05 21:19:34 -0400

    test

--- OODD_1/test/models/room_test.rb
+++ OODD_1/test/models/room_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class RoomTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @room = Room.new({room_id: 1002, building: "D.H.Hill Library", room_size: "Small - 4 chairs"})
+  end
+
+  test "should not save room without room_id" do
+    room = Room.new
+    assert_not room.save
+  end
+
+  test "should save room with room_id" do
+    assert @room.save
+  end
+
+  test "should update room with room_id" do
+    assert @room.update({room_id: 1002, building: "Hunt Library", room_size: "Small - 4 chairs"})
+  end
+
+  test "should destroy room with room_id" do
+    assert @room.destroy
+  end
 end
